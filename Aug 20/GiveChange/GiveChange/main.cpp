8b405bdd5b10e3857d582239ade4ab2a9a8e3b12
naveen <naveenspec@gmail.com>
2024-08-21 16:16:34 -0600

    modified GiveChange

--- Aug 20/GiveChange/GiveChange/main.cpp
+++ Aug 20/GiveChange/GiveChange/main.cpp
@@ -15,20 +15,49 @@ int main(int argc, const char * argv[]) {
     std::cin >> price;
     std::cout << "Enter amount paid in cents: \n";
     std::cin >> amount_paid;
+    //check if price is greater than amount paid
+    if(price > amount_paid){
+        std::cout << "Insufficient funds! \n";
+        return 1;
+    }
+    //check if price or amount_paid is negative
+    if(price < 0 || amount_paid < 0){
+        std::cout << "Error: number of cents cannot be negative \n";
+        return 1;
+    }
     //find the change
     change = amount_paid - price;
     //calculate denominations
-    quarters = change / 25;
-    difference = change % 25;
-    dimes = difference / 10;
-    difference = difference % 10;
-    nickels = difference / 5;
-    pennies = difference % 5;
 
-    std::cout << "Change = " << change << "\n";
-    std::cout << "Quarters: " << quarters << "\n";
-    std::cout << "Dimes: " << dimes << "\n";
-    std::cout << "Nickels: " << nickels << "\n";
-    std::cout << "Pennies: " << pennies << "\n";
+    quarters = change / 25;
+    if(quarters > 2){
+        quarters = 2;
+    }
+    difference = change - (quarters * 25);
+    dimes = difference / 10;
+    if(dimes > 2){
+        dimes = 2;
+    }
+    difference = difference - (dimes * 10);
+    nickels = difference / 5;
+    if(nickels > 2){
+        nickels = 2;
+    }
+    difference = difference - (nickels * 5);
+    pennies = difference / 1;
+    if(pennies > 2){
+        pennies = 2;
+    }
+    difference = difference - (pennies * 1);
+    if(difference > 0){
+        std::cout << "Unable to return change, out of coins! \n";
+    } else {
+        
+        std::cout << "Change = " << change << "\n";
+        std::cout << "Quarters: " << quarters << "\n";
+        std::cout << "Dimes: " << dimes << "\n";
+        std::cout << "Nickels: " << nickels << "\n";
+        std::cout << "Pennies: " << pennies << "\n";
+    }
     return 0;
 }
