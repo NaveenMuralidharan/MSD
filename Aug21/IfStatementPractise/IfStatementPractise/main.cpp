e35badd4782d2e0d42417c5c5c0c5e358263c55e
naveen <naveenspec@gmail.com>
2024-08-21 11:45:48 -0600

    Completed part 2 of IfStatementPractise

--- Aug21/IfStatementPractise/IfStatementPractise/main.cpp
+++ Aug21/IfStatementPractise/IfStatementPractise/main.cpp
@@ -8,7 +8,7 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    
+    //PART 1
     //declare age variable and accept input
     int age;
     std::cout << "Please enter your age: \n";
@@ -39,7 +39,49 @@ int main(int argc, const char * argv[]) {
     } else {
         std::cout << "You are part of iKid generation! \n";
     }
+    //PART 2
+    char weekdayResponse;
+    char holidayResponse;
+    char childResponse;
     
+    bool isWeekday = false;
+    bool isHoliday = false;
+    bool hasChild = false;
+    
+    std::cout << "Is this a weekday? Y/N \n";
+    std::cin >> weekdayResponse;
+    
+    std::cout << "Is this a holiday? Y/N \n";
+    std::cin >> holidayResponse;
+    
+    std::cout << "Do you have children? Y/N \n";
+    std::cin >> childResponse;
+    
+    if(weekdayResponse == 'Y'){
+        isWeekday = true;
+    }
+    
+    if(holidayResponse == 'Y'){
+        isHoliday = true;
+    }
+    
+    if(childResponse == 'Y'){
+        hasChild = true;
+    }
+    //Print whether the user gets to sleep in today or not
+    if(hasChild){
+        std::cout << "You cannot sleep in, you have children! \n";
+    } else {
+        if(!isWeekday){
+            std::cout << "You can sleep in, it's not a weekday! \n";
+        } else {
+            if(isHoliday){
+                std::cout << "You can sleep in, it's a holiday! \n";
+            } else {
+                std::cout << "You cannot sleep in, it's a weekday and not a holiday! \n";
+            }
+        }
+    }
     
     return 0;
 }
