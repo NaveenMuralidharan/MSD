2b9c2f9fbd94e93d7cacc129bc68368ada47b969
naveen <naveenspec@gmail.com>
2024-09-12 21:34:38 -0600

    completed vector template

--- Day16/MakeYourOwnVector/MakeYourOwnVector/Vector.hpp
+++ Day16/MakeYourOwnVector/MakeYourOwnVector/Vector.hpp
@@ -10,8 +10,10 @@
 #include <iostream>
 #include <stdio.h>
 
+template <typename T>
+
 class myVector{
-    int* ptr;
+    T* ptr;
     int capacity;
     int size;
     
@@ -19,26 +21,162 @@ public:
     myVector(int initialCapacity);
     myVector(const myVector & rhs);
     void operator =(const myVector & rhs);
-    int operator [](int index)const;
-    int& operator [](int index);
+    T operator [](int index)const;
+    T& operator [](int index);
     int getSize();
     int getCapacity();
     ~myVector();
-    void pushBack(int value);
+    void pushBack(T value);
     void popBack();
-    int get(int index);
-    void set(int index, int newValue);
+    T get(int index);
+    void set(int index, T newValue);
     void growVector();
-    
+    bool operator ==(const myVector& rhs)const;
+    bool operator !=(const myVector& rhs)const;
+    bool operator <(const myVector& rhs)const;
+    bool operator <=(const myVector& rhs)const;
+    bool operator >(const myVector& rhs)const;
+    bool operator >=(const myVector& rhs)const;
 };
 
-//myVector makeVector(int initialCapacity);
-//void freeVector(myVector &myVec);
-//void pushBack(myVector &myVec, int value);
-//void popBack(myVector &myVec);
-//int get(const myVector &myVec, int index);
-//void set(myVector &myVec, int index, int newValue);
-//void growVector(myVector &myVec);
+template <typename T>
 
+myVector<T>::myVector(int initialCapacity){
+
+    capacity = initialCapacity;
+    ptr = new T[capacity];
+    size = 0;
+}
+//copy constructor
+template <typename T>
+myVector<T>::myVector(const myVector<T> & rhs){
+    ptr = new T[rhs.capacity];
+    for(int i=0; i< rhs.size; i++){
+        ptr[i] = rhs.ptr[i];
+    }
+    size = rhs.size;
+    capacity = rhs.capacity;
+}
+//overload = operator
+template <typename T>
+void myVector<T>::operator =(const myVector & rhs){
+    myVector temp = myVector(rhs);
+    std::swap(size, temp.size);
+    std::swap(capacity, temp.capacity);
+    std::swap(ptr, temp.ptr);
+}
+// [] to get value at index
+template <typename T>
+T myVector<T>::operator [](int index)const{
+    return ptr[index];
+}
+//[] to set value at index
+template <typename T>
+T& myVector<T>::operator [](int index){
+    return ptr[index];
+}
+
+template <typename T>
+int myVector<T>::getSize(){
+    return size;
+}
+
+template <typename T>
+int myVector<T>::getCapacity(){
+    return capacity;
+}
+
+template <typename T>
+myVector<T>::~myVector(){
+    delete [] ptr;
+}
+
+template <typename T>
+void myVector<T>::pushBack(T value){
+    if(size == capacity){
+        growVector();
+    }
+    ptr[size] = value;
+    size ++;
+}
+
+template <typename T>
+void myVector<T>::popBack(){
+    if(size == 0){
+        size = 0;
+    } else {
+        size -= 1;
+    }
+}
+
+template <typename T>
+T myVector<T>::get(int index){
+    return ptr[index];
+}
+
+template <typename T>
+void myVector<T>::set(int index, T newValue){
+    if(index < size){
+        ptr[index] = newValue;
+    }
+}
+
+template <typename T>
+void myVector<T>::growVector(){
+    T *newPtr = new T[capacity*2];
+
+    for(int i=0; i<size; i++){
+        newPtr[i] = ptr[i];
+    }
+    delete [] ptr;
+    ptr = newPtr;
+    capacity *= 2;
+    newPtr = nullptr;
+}
+//******************
+//PART 3: Comparisons
+//==
+template <typename T>
+bool myVector<T>::operator ==(const myVector& rhs)const {
+    if(size != rhs.size){
+        return false;
+    }
+    for(int i=0; i<size; i++){
+        if(ptr[i] != rhs.ptr[i]){
+            return false;
+        }
+    }
+    return true;
+}
+//!=
+template <typename T>
+bool myVector<T>::operator !=(const myVector& rhs)const{
+    return !(*this == rhs);
+}
+// <
+template <typename T>
+bool myVector<T>::operator <(const myVector& rhs)const{
+    for(int i=0; i<size; i++){
+        if(ptr[i] < rhs.ptr[i]){
+            return true;
+        }
+    }
+    return false;
+}
+// <=
+template <typename T>
+bool myVector<T>::operator <=(const myVector& rhs)const{
+    return (*this == rhs || *this < rhs);
+}
+// >
+template <typename T>
+bool myVector<T>::operator >(const myVector& rhs)const{
+    return rhs < *this;
+}
+// >=
+template <typename T>
+bool myVector<T>::operator >=(const myVector& rhs)const{
+    return (*this == rhs || *this > rhs);
+}
 
 #endif /* Vector_hpp */
--- Day16/MakeYourOwnVector/MakeYourOwnVector/main.cpp
+++ Day16/MakeYourOwnVector/MakeYourOwnVector/main.cpp
@@ -12,20 +12,29 @@
 int main(int argc, const char * argv[]) {
     
     //test constructor
-    myVector test1Vec(4);
+    myVector<int> test1Vec(4);
     test1Vec.pushBack(10);
     test1Vec.pushBack(12);
     assert(test1Vec.getCapacity() == 4);
     assert(test1Vec.getSize() == 2);
-//
+    
+    myVector<std::string> test1strVec(4);
+    test1strVec.pushBack("hello");
+    test1strVec.pushBack("hi");
+    assert(test1strVec.getCapacity() == 4);
+    assert(test1strVec.getSize() == 2);
     
     //test pushBack
-    myVector test2Vec(3);
+    myVector<int> test2Vec(3);
     test2Vec.pushBack(80);
     assert(test2Vec.get(test2Vec.getSize()-1) == 80);
     
+    myVector<std::string> test2strVec(3);
+    test2strVec.pushBack("hello");
+    assert(test2strVec.get(test2strVec.getSize()-1) == "hello");
+    
     //test growVector
-    myVector testGrowVec(1);
+    myVector<int> testGrowVec(1);
     testGrowVec.pushBack(12);
     testGrowVec.pushBack(20);
     testGrowVec.pushBack(22);
@@ -33,50 +42,145 @@ int main(int argc, const char * argv[]) {
     testGrowVec.pushBack(45);
     assert(testGrowVec.getCapacity() == 8);
     
+    myVector<std::string> testGrowStrVec(1);
+    testGrowStrVec.pushBack("hi");
+    testGrowStrVec.pushBack("how");
+    testGrowStrVec.pushBack("are");
+    testGrowStrVec.pushBack("you");
+    testGrowStrVec.pushBack("doing");
+    assert(testGrowStrVec.getCapacity() == 8);
+    
     //test popBack
-    myVector testPopBack(3);
+    myVector<int> testPopBack(3);
     testPopBack.pushBack(10);
     testPopBack.pushBack(12);
     testPopBack.popBack();
     assert(testPopBack.getSize() == 1);
     
+    myVector<std::string> testPopBackStr(3);
+    testPopBackStr.pushBack("hi");
+    testPopBackStr.pushBack("hello");
+    testPopBackStr.popBack();
+    assert(testPopBackStr.getSize() == 1);
+    
     //test get
     assert(testGrowVec.get(2) == 22);
-
+    assert(testGrowStrVec.get(2) == "are");
+    
     //test set
-    myVector testSetVec(4);
+    myVector<int> testSetVec(4);
     testSetVec.pushBack(12);
     testSetVec.set(0, 10);
     assert(testSetVec.get(0)==10);
     
+    myVector<std::string> testSetStrVec(4);
+    testSetStrVec.pushBack("hello");
+    testSetStrVec.set(0, "hi");
+    assert(testSetStrVec.get(0)=="hi");
+    
     //test copy constructor
-    myVector copy(4);
+    myVector<int> copy(4);
     copy.pushBack(1);
     copy.pushBack(2);
     
-    myVector testCopy(copy);
+    myVector<int> testCopy(copy);
     assert(copy.getSize() == testCopy.getSize());
     assert(copy.get(0) == testCopy.get(0));
     testCopy.pushBack(3);
     assert(copy.get(2) != testCopy.get(2));
     
+    myVector<std::string> strCopy(4);
+    strCopy.pushBack("hello");
+    strCopy.pushBack("hi");
+    
+    myVector<std::string> testStrCopy(strCopy);
+    assert(strCopy.getSize() == testStrCopy.getSize());
+    assert(strCopy.get(0) == testStrCopy.get(0));
+    testStrCopy.pushBack("bonjour");
+    assert(strCopy.get(2) != testStrCopy.get(2));
+    
     // test = operator overload
-    myVector copy1(4);
+    myVector<int> copy1(4);
     copy1.pushBack(1);
     copy1.pushBack(2);
     
-    myVector copy2(6);
+    myVector<int> copy2(6);
     copy2.pushBack(3);
     copy2.pushBack(4);
     copy1 = copy2;
     assert(copy1.get(0)==copy2.get(0));
     assert(copy1.getSize()==copy2.getSize());
+    
+    myVector<std::string> strCopy1(4);
+    strCopy1.pushBack("hello");
+    strCopy1.pushBack("hi");
+    
+    myVector<std::string> strCopy2(6);
+    strCopy2.pushBack("bonjour");
+    strCopy2.pushBack("namaste");
+    strCopy1 = strCopy2;
+    assert(strCopy1.get(0)==strCopy2.get(0));
+    assert(strCopy1.getSize()==strCopy2.getSize());
+    
     //test [] get value
     assert(copy2[0] == 3);
+    assert(strCopy2[0] == "bonjour");
+    
     //test [] set value
     copy2[0] = 10;
     assert(copy2[0] == 10);
-
+    
+    strCopy2[0] = "world";
+    assert(strCopy2[0] == "world");
+    
+    //test == operator overload
+    myVector<int> testDblEq1(4);
+    testDblEq1.pushBack(1);
+    testDblEq1.pushBack(2);
+    testDblEq1.pushBack(3);
+    
+    myVector<int> testDblEq2(6);
+    testDblEq2.pushBack(1);
+    testDblEq2.pushBack(2);
+    testDblEq2.pushBack(3);
+    
+    assert(testDblEq1 == testDblEq2);
+    
+    //test != operator overload
+    myVector<int> testNotEq1(4);
+    testNotEq1.pushBack(1);
+    testNotEq1.pushBack(2);
+    testNotEq1.pushBack(3);
+    testNotEq1.pushBack(5);
+    assert(testDblEq1 != testNotEq1);
+    
+    //test < operator
+    myVector<int> testLess1(4);
+    testLess1.pushBack(9);
+    testLess1.pushBack(9);
+    testLess1.pushBack(4);
+    testLess1.pushBack(15);
+    
+    myVector<int> testLess2(4);
+    testLess2.pushBack(9);
+    testLess2.pushBack(9);
+    testLess2.pushBack(10);
+    testLess2.pushBack(12);
+    
+    assert(testLess1 < testLess2);
+    assert(!(testDblEq1 < testDblEq2));
+    
+    //test <= operator
+    assert(testLess1 <= testLess2);
+    assert(testDblEq1 <= testDblEq2);
+    
+    //test > operator
+    assert(testLess2 > testLess1);
+    assert(!(testDblEq1 > testDblEq2));
+    
+    //test >= operator
+    assert(testLess2 >= testLess1);
+    assert(testDblEq1 >= testDblEq2);
     
     return 0;
 }
